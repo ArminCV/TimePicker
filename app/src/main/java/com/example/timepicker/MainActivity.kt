edf79f8d3c8cadaa2474010b901b0889e7e66670
armincvaldez <armincvaldez@gmail.com>
2020-07-07 23:38:58 +0800

    Initial Time Picker Activity

---
+++ app/src/main/java/com/example/timepicker/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.timepicker
+
+import android.app.TimePickerDialog
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import java.util.*
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun clickTimePicker(view:View){
+        val c = Calendar.getInstance();
+        val hour = c.get(Calendar.HOUR);
+        val minute = c.get(Calendar.MINUTE);
+        val tpd = TimePickerDialog(this,
+            TimePickerDialog.OnTimeSetListener(
+                function = {
+                    view, h, m -> Toast.makeText(this,
+                    "$h : $m : ",
+                    Toast.LENGTH_LONG).show()
+                }
+            ), hour, minute, false);
+        tpd.show();
+    } // function end
+
+}
